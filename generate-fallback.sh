c2d9a5c774150a008234b9b9627c34295f2d690b
Matthew Tylee Atkinson <matatk@gmail.com>
2014-06-18 15:56:56 +0100

    The element formerly known as...
    * Rename to the more apt (yet still short) "html5-h" (closes #23)
    * Update borken link in demo-fallback.html

--- generate-fallback.sh
+++ generate-fallback.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# Generate the fallback version of w3c-h that extends h1 from the main version
-SRC=w3c-h.html
-OUT=fallback/w3c-h.html
+# Generate the fallback version of html5-h that extends h1 from the main version
+SRC=html5-h.html
+OUT=fallback/html5-h.html
 cat $SRC | \
 	sed 's/components\/polymer/..\/components\/polymer/' | \
 	sed 's/attributes="level"/attributes="level" extends="h1"/' | \
